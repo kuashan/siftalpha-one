75f1cf0c65346726a83b396b56b986aa1ac38c28
kuashan <159182376+kuashan@users.noreply.github.com>
2026-09-18 09:31:15 -0700

    chore: version alpha43 embedded dependency hotfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "com.siftalpha.studio"
         minSdk = 26
         targetSdk = 36
-        versionCode = 119
-        versionName = "0.8.0-alpha43"
+        versionCode = 127
+        versionName = "0.8.0-alpha43-r1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
