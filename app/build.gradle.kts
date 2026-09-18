7521858eaad43cfc03da2c91557744a60f63c162
kuashan <159182376+kuashan@users.noreply.github.com>
2026-09-18 10:11:35 -0700

    chore: version alpha43 idle status hotfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "com.siftalpha.studio"
         minSdk = 26
         targetSdk = 36
-        versionCode = 128
-        versionName = "0.8.0-alpha43-r2"
+        versionCode = 129
+        versionName = "0.8.0-alpha43-r3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
