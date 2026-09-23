595fdf3eb9ca310025af0e441132c49f39008f68
kuashan <159182376+kuashan@users.noreply.github.com>
2026-09-23 09:36:14 +0800

    build: enable Kotlin JVM core module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
     id("com.android.application") version "9.1.1" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.2.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
 }
